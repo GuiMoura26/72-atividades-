c01d4175bc29bbbd58d8b9c71530b642f947a539
Guilherme <guilhermemouraa5@gmail.com>
2019-08-14 14:35:35 -0300

    Adicinar Calendario

---
+++ valores.py
@@ -0,0 +1,16 @@
+v_1 = int(input('Digite o valor 1'))
+v_2 = int(input('Digite o valor 2'))
+
+def soma (v_1, v_1)
+    return(v_1 + v_2)
+
+def sub (v_1, v_1)
+    return(v_1 - v_2)
+
+def div (v_1, v_1)
+    return(v_1/v_2)
+
+def mult (v_1, v_1)
+    return(v_1 * v_2)
+
+
